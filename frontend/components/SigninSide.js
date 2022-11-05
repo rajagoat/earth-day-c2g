3bc67e4228e826321668c3e77dcf65a9591ae5f9
MountainBikerYYC <ywang9696@gmail.com>
2022-11-04 18:24:37 -0600

    changed background image, added gleaning title

--- frontend/components/SigninSide.js
+++ frontend/components/SigninSide.js
@@ -48,14 +48,14 @@ export default function SignInSide() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random)',
+            backgroundImage: 'url(https://images3.alphacoders.com/190/thumbbig-190787.webp)',
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
             backgroundSize: 'cover',
             backgroundPosition: 'center',
-          }}
-        />
+          }}>
+        </Grid>
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <Box
             sx={{
@@ -66,6 +66,9 @@ export default function SignInSide() {
               alignItems: 'center',
             }}
           >
+             <Typography component="h1" variant="h5">
+              Welcome to Gleaning
+            </Typography>
             <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
               <LockOutlinedIcon />
             </Avatar>
