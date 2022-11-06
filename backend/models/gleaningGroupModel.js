5446f5084e069dcc9abd33b895ab9a0694509642
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2022-11-06 01:43:13 -0700

    removed group validation, handle in frontend instead

--- backend/models/gleaningGroupModel.js
+++ backend/models/gleaningGroupModel.js
@@ -9,13 +9,8 @@ const gleaningGroupSchema = new Schema({
     },
     gleaners: [{
         type: Schema.Types.ObjectId, ref: 'Gleaner',
-        required: true,
-        validate: [groupMinimum, '{PATH} fails to meet the minimum group size of 5']
+        required: true
     }],
 });
-
-groupMinimum = (val) => {
-    return val.length >= 5;
-}
 
 module.exports = mongoose.model('Gleaning Group', gleaningGroupSchema);
