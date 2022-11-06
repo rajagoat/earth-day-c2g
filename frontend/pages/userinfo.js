9344c2582e91168d6bfa5e4db8442c0d310c9cd8
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2022-11-06 03:46:32 -0700

    Added form creation requirements

--- frontend/pages/userinfo.js
+++ frontend/pages/userinfo.js
@@ -7,8 +7,15 @@ const userInfo = () => {
             <p>Agree to rules + legal information</p>
             <p>Availabilities</p>
             <h2>Gleaners need:</h2>
-            <p>Select a group</p>
-            
+            <p>Select a group from the list (ideally 
+                sorted by locality)
+            </p>
+            <p>distance (range) they're willing to go</p>
+            <h2>Farmers need:</h2>
+            <p>Maximum capacity of produce they can collect</p>
+            <h2>Food banks need:</h2>
+            <p>distance (range) they're willing to go</p>
+            <p>Maximum capacity of produce they can collect</p>
         </div>
     );
 }
