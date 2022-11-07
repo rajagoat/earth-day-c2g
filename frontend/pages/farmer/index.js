6b2509f106a2d286a59173e0ca2f563562638cb0
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2022-11-07 12:45:20 -0700

    formatted dates in farmer

--- frontend/pages/farmer/index.js
+++ frontend/pages/farmer/index.js
@@ -43,18 +43,8 @@ const Home = () => {
                                 </div>
                             </a>
                         </div>
-                        {/* <button className="focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600 mt-4 sm:mt-0 inline-flex items-start justify-start px-6 py-3 bg-indigo-700 hover:bg-indigo-600 focus:outline-none rounded"> 
-                            <p className="text-sm font-medium leading-none text-white">Add Gleaning Activity</p> */}
                         <GleaningModal />
-                        {/* </button> */}
-
-
-
-
-
                     </div>
-
-
                     <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
                         <table className="items-center w-full bg-transparent border-collapse">
                             <thead>
@@ -68,114 +58,6 @@ const Home = () => {
                                 </tr>
                             </thead>
                             <GleaningActivities />
-                            {/* <tbody>
-                            <tr>
-                                <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
-                                    <img src="https://demos.creative-tim.com/notus-nextjs/img/bootstrap.jpg" className="h-12 w-12 bg-white rounded-full border" alt="..." />
-                                    <span className="ml-3 font-bold NaN">Farm Name(25 lbs): We have some fresh fruits to offer all</span>
-                                </th>
-                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">21st Nov, 2022</td>
-                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                    <i className="fas fa-circle text-orange-500 mr-2"></i> Active
-                                </td>
-                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                    <i className="fas fa-circle text-orange-500 mr-2"></i> H0H - H9X, Montreal 
-                                </td>
-                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                    <div className="flex items-center">
-                                        <span className="mr-2">60%</span>
-                                        <div className="relative w-full">
-                                            <div className="overflow-hidden h-2 text-xs flex rounded bg-red-200">
-                                                <div className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-500" style={{width: "60%"}}></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
-                                    <Link className="text-blueGray-500 py-1 px-3" href="#pablo">
-                                        <i className="fas fa-ellipsis-v"></i>
-                                    </Link>
-                                    <div className="hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48" style={{position: "absolute", inset: "0px 0px auto auto", margin: "0px", transform: "translate(-108px, 414px)"}} data-popper-placement="left-start">
-                                        <Link href="#pablo" className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Action</Link>
-                                        <Link href="#pablo" className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Another action</Link>
-                                        <Link href="#pablo" className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Something else here</Link>
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
-                                    <img src="https://demos.creative-tim.com/notus-nextjs/img/angular.jpg" className="h-12 w-12 bg-white rounded-full border" alt="..." />
-                                    <span className="ml-3 font-bold NaN">Farm Name(50 lbs): We are having fresh Vegiies and fruits to offer most welcome to pick up.</span>
-                                </th>
-                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">14th Nov, 2022</td>
-                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                    <i className="fas fa-circle text-emerald-500 mr-2"></i> Active
-                                </td>
-                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                    <i className="fas fa-circle text-orange-500 mr-2"></i> H0H - H9X, Montreal 
-                                </td>
-                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                    <div className="flex items-center">
-                                        <span className="mr-2">100%</span>
-                                        <div className="relative w-full">
-                                            <div className="overflow-hidden h-2 text-xs flex rounded bg-emerald-200">
-                                                <div className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-emerald-500" style={{width: "100%"}}></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
-                                    <Link className="text-blueGray-500 py-1 px-3" href="#pablo">
-                                        <i className="fas fa-ellipsis-v"></i>
-                                    </Link>
-                                    <div className="hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48" style={{position: "absolute", inset: "0px 0px auto auto", margin: "0px", transform: "translate(-108px, 494px)"}} data-popper-placement="left-start">
-                                        <Link href="#pablo" className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Action</Link>
-                                        <Link href="#pablo" className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Another action</Link>
-                                        <Link href="#pablo" className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Something else here</Link>
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
-                                    <img src="https://demos.creative-tim.com/notus-nextjs/img/sketch.jpg" className="h-12 w-12 bg-white rounded-full border" alt="..." />
-                                    <span className="ml-3 font-bold NaN">Farm Name(75 lbs): Fresh Tomatos avalable</span>
-                                </th>
-                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">24th Nov, 2022</td>
-                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                    <i className="fas fa-circle text-red-500 mr-2"></i> Active
-                                </td>
-                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                    <i className="fas fa-circle text-orange-500 mr-2"></i> H0H - H9X, Montreal 
-                                </td>
-                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                    <div className="flex items-center">
-                                        <span className="mr-2">73%</span>
-                                        <div className="relative w-full">
-                                            <div className="overflow-hidden h-2 text-xs flex rounded bg-red-200">
-                                                <div className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-500" style={{width: "73%"}}></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
-                                    <Link className="text-blueGray-500 py-1 px-3" href="#pablo">
-                                        <i className="fas fa-ellipsis-v"></i>
-                                    </Link>
-                                    <div className="hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48">
-                                        <Link href="#pablo" className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Action</Link>
-                                        <Link href="#pablo" className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Another action</Link>
-                                        <Link href="#pablo" className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Something else here</Link>
-                                    </div>
-                                </td>
-
-                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                        <CheckRating/>
-                                </td>
-                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                        <Foodbankinfo/>
-                                </td>
-                            </tr>
-                        </tbody> */}
                         </table>
                     </div>
 
