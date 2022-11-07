422eed1db5d2aee1cce27c96cf27ae42654fae3b
Ameen Y <113547608+ameeny@users.noreply.github.com>
2022-11-06 17:03:13 -0700

    Created profile template

---
+++ frontend/pages/profile.js
@@ -0,0 +1,82 @@
+import Navbar from '../components/Navbar'
+
+
+
+const Profile = () => {
+    return (
+        <div>
+            <Navbar />
+            
+
+            <div className="overflow-hidden bg-white shadow sm:rounded-lg">
+                <div className="px-4 py-5 sm:px-6">
+                    <h3 className="text-lg font-medium leading-6 text-gray-900">User Profile</h3>
+                    <p className="mt-1 max-w-2xl text-sm text-gray-500">Personal details and application.</p>
+                </div>
+                <div className="border-t border-gray-200">
+                    <dl>
+                        <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Full Name</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Jane Doe</dd>
+                        </div>
+                        <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Username</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Gleaner</dd>
+                        </div>
+                        <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Email address</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">happygleaner@example.com</dd>
+                        </div>
+                        <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Location</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Calgary, AB</dd>
+                        </div>
+                        <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">About</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                                Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur
+                                qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud
+                                pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
+                            </dd>
+                        </div>
+                        <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Recent Activity</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                                <ul role="list" className="divide-y divide-gray-200 rounded-md border border-gray-200">
+                                    <li className="flex items-center justify-between py-3 pl-3 pr-4 text-sm">
+                                        <div className="flex w-0 flex-1 items-center">
+                                            {/* <PaperClipIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" /> */}
+                                            <span className="ml-2 w-0 flex-1 truncate">XYZ Gleaning Activity</span>
+                                        </div>
+                                        <div className="ml-4 flex-shrink-0">
+                                            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                                View
+                                            </a>
+                                        </div>
+                                    </li>
+                                    <li className="flex items-center justify-between py-3 pl-3 pr-4 text-sm">
+                                        <div className="flex w-0 flex-1 items-center">
+                                            {/* <PaperClipIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" /> */}
+                                            <span className="ml-2 w-0 flex-1 truncate">XYZ Gleaning Activity</span>
+                                        </div>
+                                        <div className="ml-4 flex-shrink-0">
+                                            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                                View
+                                            </a>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </dd>
+                        </div>
+                    </dl>
+                </div>
+            </div>
+
+
+
+        </div>
+
+    );
+}
+
+export default Profile;
